c593f1d40f1b524729dc7cda87bfada9d826b51c
cuiygfighting <cuiyg@pku.edu.cn>
2020-04-27 22:37:24 +0800

    add new file

---
+++ 笔面试遇到的题目/腾讯2020春招实习/用两个栈实现队列.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+#include<string>
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	void poll() {
+		int temp;
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				temp = stack1.top();
+				stack2.push(temp);
+				stack1.pop();
+			}
+		}
+		//temp = stack2.top();
+		stack2.pop();
+		//return temp;
+	}
+
+	int peek() {
+		int temp;
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				temp = stack1.top();
+				stack2.push(temp);
+				stack1.pop();
+			}
+		}
+		temp = stack2.top();
+		//stack2.pop();
+		return temp;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
+
+int main()
+{
+	string str;
+	int number;
+	Solution que;
+	while (cin >> str )
+	{
+		if (str == "add")
+		{
+			cin >> number;
+			que.push(number);
+		}
+		else if (str == "poll") que.poll();
+		else if (str == "peek")
+		{
+			int tmp = que.peek();
+			cout << tmp<<endl;
+		}
+	}
+	return 0;
+}
